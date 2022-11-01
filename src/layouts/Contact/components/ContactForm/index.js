46bd04909f191b553b51adf2d42aabd9bd2bd651
jpceci <cecilia_jp@live.com>
2022-11-01 13:55:05 -0400

    onSubmit de contact terminado

--- src/layouts/Contact/components/ContactForm/index.js
+++ src/layouts/Contact/components/ContactForm/index.js
@@ -18,90 +18,108 @@ import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 
-
 function ContactForm({ userData }) {
   const [medio, setMedio] = useState("");
   const [value, setValue] = React.useState(null);
+  const [content, setContent] = React.useState("");
 
   const handleChange = (event) => {
     setMedio(event.target.value);
   };
+
+  const contChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(medio);
+    console.log(value);
+    console.log(content);
+  };
+
   return (
     <Card style={{ height: 400, width: 700 }}>
-      <MDBox pt={3} px={3}>
-        <Grid item xs container direction="row">
-          <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
-            Usuario: 
-          </MDTypography>
-          <MDTypography variant="subtitle2" color="#text" fontWeight="regular">
-            {userData.firstName + " " + userData.lastName}
-          </MDTypography>
-        </Grid>
-        <Grid item xs container direction="row" sx={{ marginTop: 1 }}>
-          <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
-            Fecha:
-          </MDTypography>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-          {/* <TextField id="outlined-basic"  variant="outlined" /> */}
-        </Grid>
-        <Grid item xs container direction="row" sx={{ marginTop: 1 }}>
-          <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
-            Medio:
-          </MDTypography>
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth sx={{ margin: 2 }}>
-              <InputLabel id="outlined-basic"></InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Medio"
-                value={medio}
-                onChange={handleChange}
-                autoWidth
-              >
-                <MenuItem value={"facebook"}>Facebook</MenuItem>
-                <MenuItem value={"email"}>Email</MenuItem>
-                <MenuItem value={"telefono"}>Telefono</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Grid>
-        <hr />
-      </MDBox>
+      <form onSubmit={handleSubmit}>
+        <MDBox pt={3} px={3}>
+          <Grid item xs container direction="row">
+            <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
+              Usuario:
+            </MDTypography>
+            <MDTypography variant="subtitle2" color="#text" fontWeight="regular">
+              {userData.firstName + " " + userData.lastName}
+            </MDTypography>
+          </Grid>
+          <Grid item xs container direction="row" sx={{ marginTop: 1 }}>
+            <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
+              Fecha:
+            </MDTypography>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            {/* <TextField id="outlined-basic"  variant="outlined" /> */}
+          </Grid>
+          <Grid item xs container direction="row" sx={{ marginTop: 1 }}>
+            <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
+              Medio:
+            </MDTypography>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth sx={{ margin: 2 }}>
+                <InputLabel id="outlined-basic"></InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="Medio"
+                  value={medio}
+                  onChange={handleChange}
+                  autoWidth
+                >
+                  <MenuItem value={"facebook"}>Facebook</MenuItem>
+                  <MenuItem value={"email"}>Email</MenuItem>
+                  <MenuItem value={"telefono"}>Telefono</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Grid>
+          <hr />
+        </MDBox>
 
-      <MDBox pt={3} px={3}>
-        <Grid item xs container direction="row">
-          <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
-            Contenido:
-          </MDTypography>
-        </Grid>
-        <Grid item xs container direction="row">
-          <TextField
-            id="outlined-multiline-static"
-            multiline
-            rows={4}
-            fullWidth
-          />
-        </Grid>
-        <MDButton
-          style={{ position: "absolute", bottom: 15, maxWidth: "80%" }}
-          variant="gradient"
-          color="info"
-          size="small"
-          href="/dashboard"
-        >
-          Guardar
-        </MDButton>
-      </MDBox>
+        <MDBox pt={3} px={3}>
+          <Grid item xs container direction="row">
+            <MDTypography variant="subtitle2" color="dark" fontWeight="medium">
+              Contenido:
+            </MDTypography>
+          </Grid>
+          <Grid item xs container direction="row">
+            <TextField
+              id="outlined-multiline-static"
+              multiline
+              rows={4}
+              type="text"
+              onChange={contChange}
+              fullWidth
+            />
+          </Grid>
+          <MDButton
+            style={{ position: "absolute", bottom: 15, maxWidth: "80%" }}
+            variant="gradient"
+            color="info"
+            size="small"
+            type="submit"
+          >
+            Guardar
+          </MDButton>
+        </MDBox>
+      </form>
     </Card>
   );
 }
+
+
 export default ContactForm;
--- src/layouts/CrearPromocion/components/PromotionForm/index.js
+++ src/layouts/CrearPromocion/components/PromotionForm/index.js
@@ -6,65 +6,101 @@ import MDTypography from "../../../../components/MDTypography";
 import TextField from "@mui/material/TextField";
 import MultipleSelectCheckmarks from "../MultipleSelectCheckMarks";
 import MDButton from "../../../../components/MDButton";
+import { useState } from "react";
 import * as React from "react";
 
 let products = ["producto1", "prod2", "prod3"];
 function PromotionForm() {
+  const [description, setDescription] = useState("");
+  const [link, setLink] = useState("");
+  const [discount, setDiscount] = React.useState("");
+  const [value, setProduct] = React.useState("");
+
+  const handleChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const linkChange = (event) => {
+    setLink(event.target.value);
+  };
+
+  const discChange = (event) => {
+    setDiscount(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(discount);
+    console.log(value);
+    console.log(description);
+    console.log(link);
+  };
+
   return (
     <Card style={{ height: 500, width: 700 }}>
-      <MDBox pt={3} px={3}>
-        <hr />
+      <form onSubmit={handleSubmit}>
+        <MDBox pt={3} px={3}>
+          <hr />
 
-        <Grid item xs container direction="row" sx={{ marginTop: 1 }}>
-          <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
-            Producto(s):
-          </MDTypography>
-          <Box sx={{ minWidth: 120 }}>
-            <MultipleSelectCheckmarks products={products} />
-          </Box>
-          {/* <TextField id="outlined-basic"  variant="outlined" /> */}
-        </Grid>
-        <Grid item xs container direction="row">
-          <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
-            Descuento:
-          </MDTypography>
-          <Box sx={{ minWidth: 120 }}>
-              <TextField variant="outlined" size="small" /> %
-          </Box>
-        
-        </Grid>
-      </MDBox>
+          <Grid item xs container direction="row" sx={{ marginTop: 1 }}>
+            <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
+              Producto(s):
+            </MDTypography>
+            <Box sx={{ minWidth: 120 }}>
+              <MultipleSelectCheckmarks
+                products={products}
+              />
+            </Box>
+            {/* <TextField id="outlined-basic"  variant="outlined" /> */}
+          </Grid>
+          <Grid item xs container direction="row">
+            <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
+              Descuento:
+            </MDTypography>
+            <Box sx={{ minWidth: 120 }}>
+              <TextField variant="outlined" size="small" type="text" onChange={discChange} /> %
+            </Box>
 
-      <MDBox pt={3} px={3}>
-        <Grid item xs container direction="row">
-          <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
-            Descripcion:
-          </MDTypography>
-        </Grid>
-        <Grid item xs container direction="row">
-          <TextField
-            id="outlined-multiline-static"
-            multiline
-            rows={4}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs container direction="row">
-          <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
-            Foto (link):
-          </MDTypography>
-          <TextField fullWidth />
-        </Grid>
-        <MDButton
-          style={{ position: "absolute", bottom: 15, maxWidth: "80%" }}
-          variant="gradient"
-          color="info"
-          size="small"
-          href="http://localhost:3000/dashboard"
-        >
-          Guardar
-        </MDButton>
-      </MDBox>
+          </Grid>
+        </MDBox>
+
+        <MDBox pt={3} px={3}>
+          <Grid item xs container direction="row">
+            <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
+              Descripcion:
+            </MDTypography>
+          </Grid>
+          <Grid item xs container direction="row">
+            <TextField
+              id="outlined-multiline-static"
+              multiline
+              rows={4}
+              type="text"
+              onChange={handleChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs container direction="row">
+            <MDTypography variant="subtitle2" color="#fff" fontWeight="medium">
+              Foto (link):
+            </MDTypography>
+            <TextField
+              type="text"
+              onChange={linkChange}
+              fullWidth />
+          </Grid>
+          <MDButton
+            style={{ position: "absolute", bottom: 15, maxWidth: "80%" }}
+            variant="gradient"
+            color="info"
+            size="small"
+            type="submit"
+          // href="http://localhost:3000/dashboard"
+          >
+            Guardar
+          </MDButton>
+        </MDBox>
+      </form>
     </Card>
   );
 }
