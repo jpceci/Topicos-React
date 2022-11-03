94df8e29c385e0caef886259a703c1b96d5392ee
jpceci <cecilia_jp@live.com>
2022-11-02 23:04:34 -0400

    login terminado

--- src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
+++ src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
@@ -45,10 +45,10 @@ function DefaultNavbarMobile({ open, close }) {
       MenuListProps={{ style: { width: `calc(${width}px - 4rem)` } }}
     >
       <MDBox px={0.5}>
-        <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" />
-        <DefaultNavbarLink icon="person" name="profile" route="/profile" />
-        <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" />
-        <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" />
+        <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" light={true}/>
+        <DefaultNavbarLink icon="person" name="profile" route="/profile" light={true} />
+        <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" light={true} />
+        <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" light={true}/>
       </MDBox>
     </Menu>
   );
